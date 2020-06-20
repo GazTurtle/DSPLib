7c968e22388a8583ab86a2f0bddd0e05fcd6f2d3
Gaz Rowntree <gazrideguide@googlemail.com>
2020-06-20 22:43:38 +0100

    Update adc_dac-interupts.ino
    Made the Yummy DSP work with the interupt sampling version! WOW!

--- examples/adc_dac-interupts/adc_dac-interupts.ino
+++ examples/adc_dac-interupts/adc_dac-interupts.ino
@@ -5,33 +5,32 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "driver/i2s.h"
 #include "driver/adc.h"
 #include "driver/dac.h"
 
 
-#include "soc/syscon_reg.h"
+//#include "soc/syscon_reg.h"
 
 //for fast adc sampling via timer interupts
 #include <soc/sens_reg.h>
 #include <soc/sens_struct.h>
 
-//https://github.com/espressif/esp-idf/blob/master/examples/peripherals/i2s_adc_dac/main/app_main.c
+#include <YummyDSP.h>
+
+YummyDSP dsp;
+FilterNode lp;
+FilterNode hp;
+WaveShaperNode sat;
+DelayNode dly;
+
+#define POT_PIN A5
+float pot=0.f;
 
 //https://github.com/espressif/arduino-esp32/blob/master/libraries/ESP32/examples/Timer/RepeatTimer/RepeatTimer.ino
 
-//i2s number
-#define I2S_NUM                         (i2s_port_t)(0)
 
-#define BUFFER_LEN                      6000
+#define BUFFER_LEN                      32 //6000
 
-#define CHANNELS                        2
-
-//https://www.esp32.com/viewtopic.php?t=5574
-
-
-
-//uint16_t* i2s_write_buff = (uint16_t*) calloc(BUFFER_LEN * CHANNELS, sizeof(uint16_t));
 
 #define NUMBER_OF_BUFFERS   3
 
@@ -70,9 +69,17 @@ void complexHandler(void *param)
           //eventually this will grow to a proper filter and stuff :)
         
           //convert from 12 bit adc reading to 8 bit for dac output
-          float sample = float(abuf[SampleBufferToProcess][i]) / 4095.0f;
+          float sample = float(abuf[SampleBufferToProcess][i]) / 2047.0f;
 
-          uint16_t uintSample = uint16_t(sample * 255.0f); 
+          sample -= 1; //offset down around actual zero
+
+          //process the sample
+          uint16_t ch = 0; //only one channel for now
+          sample = dsp.process(sample, ch);
+
+          //now scale to 8bits for output to DAC
+          sample += 1; //offset back up for output to DAC
+          uint16_t uintSample = uint16_t(sample * 120.0f); 
 
           //store the sample back, and move on
           abuf[SampleBufferToProcess][i] = uintSample;
@@ -87,38 +94,7 @@ void complexHandler(void *param)
     }
   }
 }
-//void complexHandler(void *param) 
-//{
-//  while (true)
-//  {
-//    // Sleep until the ISR gives us something to do, or for 1 second
-//    uint32_t tcount = ulTaskNotifyTake(pdFALSE, pdMS_TO_TICKS(1000));  
-//
-//    if (SampleBufferReady==true) 
-//    {
-//      SampleBufferReady=false;
-//      
-//      size_t bytes_written=0;
-//      
-//      uint16_t j=0;
-//    
-//      for(int i = 0; i < BUFFER_LEN; i++)
-//      { 
-//          //convert from 12 bit adc reading to 8 bit for dac output
-//          float sample = float(abuf[SampleBufferToRead][i]) / 4095.0f;
-//
-//          uint16_t uintSample = uint16_t(sample * 255.0f) << 8; // the DAC reads 8 bits of MSB only, so we take the 0-256 value and shove it up top
-//
-//          //turn this mono signal into stereo
-//          //send to right and left channels
-//          i2s_write_buff[j++] = uint16_t(uintSample);//endian-ness is msb written to lowest memory location first.
-//          i2s_write_buff[j++] = uint16_t(uintSample);
-//      }
-//  
-//      //i2s_write(I2S_NUM, i2s_write_buff, BUFFER_LEN * CHANNELS, &bytes_written, portMAX_DELAY);
-//    }
-//  }
-//}
+
 
 
 
@@ -195,54 +171,15 @@ void setupInterupts()
 {
   //Serial.println("setupInterupts...");
   //run the dsp on the other processor.
-  //xTaskCreatePinnedToCore(complexHandler, "Handler Task", 8192, NULL, 1, &complexHandlerTask, 0);
+  xTaskCreatePinnedToCore(complexHandler, "Handler Task", 8192, NULL, 1, &complexHandlerTask, 1);
 
-  xTaskCreate(complexHandler, "Handler Task", 1000/*8192*/, NULL, 1, &complexHandlerTask);
+  //xTaskCreate(complexHandler, "Handler Task", 1000/*8192*/, NULL, 1, &complexHandlerTask);
   adcTimer = timerBegin(3, 80, true); // 80 MHz timer clock / 80 = 1 MHz hardware clock for easy figuring, means counter is microseconds
   timerAttachInterrupt(adcTimer, &onTimer, true); // Attaches the handler function to the timer 
-  timerAlarmWrite(adcTimer, 200, true); // Interrupts when counter == 50 microseconds, frequency is 1 / (50 * 10^-6) , i.e. 20,000 times a second (20khz)
+  timerAlarmWrite(adcTimer, 50, true); // Interrupts when counter == 50 microseconds, frequency is 1 / (50 * 10^-6) , i.e. 20,000 times a second (20khz)
   timerAlarmEnable(adcTimer);
 }
 
-
-/**
- * @brief I2S ADC/DAC mode init.
- */
-void i2s_init(void)
-{
-//     i2s_port_t i2s_num = I2S_NUM;
-//    
-//
-//    i2s_config_t i2s_config;
-//    i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN);
-//    i2s_config.sample_rate = 20000;//BUFFER_LEN * 16;//11025;//48000; //11025; // 96000;//44100;           
-//                       
-//    i2s_config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
-//    i2s_config.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT; //I2S_CHANNEL_FMT_RIGHT_LEFT;  //I2S_CHANNEL_FMT_ONLY_LEFT
-//    i2s_config.use_apll = false;
-//    i2s_config.communication_format = (i2s_comm_format_t) ( /*I2S_COMM_FORMAT_PCM |*/ I2S_COMM_FORMAT_I2S_MSB); //I2S_COMM_FORMAT_PCM;
-//    i2s_config.intr_alloc_flags = 0;//ESP_INTR_FLAG_LEVEL3; //ESP_INTR_FLAG_LEVEL3; // high interrupt priority //0;
-//    i2s_config.dma_buf_count = 64;
-//    i2s_config.dma_buf_len = BUFFER_LEN * CHANNELS;
-//    //i2s_config.tx_desc_auto_clear = true;
-//    
-//     //install and start i2s driver
-//     i2s_driver_install(i2s_num, &i2s_config, 0, NULL);
-//     //init DAC pad, the above line does this anyway
-//     i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN);
-//
-//
-//    i2s_set_sample_rates(i2s_num, 20000);
-//
-//    SET_PERI_REG_MASK(SYSCON_SARADC_CTRL2_REG, SYSCON_SARADC_SAR1_INV); //MADE NO DIFFERENCE
-//
-//    i2s_zero_dma_buffer(i2s_num);
-//
-//    //init DAC pad
-//    i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN);
-     
-
-}
 
 
 void setup()
@@ -257,10 +194,36 @@ void setup()
   adc1_config_width(ADC_WIDTH_BIT_12);
   adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11);
   int val = adc1_get_raw(ADC1_CHANNEL_0);
-  
-  //i2s_init();
+
+  uint16_t fs = 40000;
+  uint16_t channelCount = 1;
+
+  // setup audio lib
+  dsp.begin(fs);
+
+  // setup some filter nodes
+  lp.begin(fs, channelCount);
+  lp.setupFilter(FilterNode::LPF, 2000, 0.7);
+
+  hp.begin(fs, channelCount);
+  hp.setupFilter(FilterNode::HPF, 30, 1.0);
+
+  dly.begin(fs, channelCount);
+  dly.setDelayMs(dly.maxDelayMs());
+
+  sat.begin(fs, channelCount);
+  sat.setDrive(0.2f);
+
+  // add nodes to audio processing tree
+  // I2S in => hp => delay => saturation => lp => I2S out
+  dsp.addNode(&hp);
+  dsp.addNode(&dly);
+  //dsp.addNode(&sat);
+  dsp.addNode(&lp);
+
   
   setupInterupts();
+
 
 }
 
@@ -268,4 +231,24 @@ void setup()
 void loop()
 {
 
+  // sample the potentiometer multiple times, adc is too noisy 
+  int readCnt = 50;
+  float p = analogRead(POT_PIN);
+  for (int i=0; i<readCnt; i++)
+  {
+    p += analogRead(POT_PIN);
+  }
+  
+  
+
+  
+  // low pass filtered and scaled between 0-1
+  pot = pot * 0.99 + (float)p/4096/(1+readCnt) * 0.01;
+
+ 
+  //lp.updateFilter(pot * 10000.f);
+  //sat.setDrive(pot);
+  dly.setDelayMs(pot * dly.maxDelayMs());
+  //hp.updateFilter(pot * 10000.f);
+  
 }
